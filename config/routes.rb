ab8b0553264ad89917e485f69dbeb34b7659b70c
Prasanna Vijayan <prasanna.vijayan@icicletech.com>
2016-02-25 16:29:24 +0530

    Devise plugin added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :expenses
 
   get 'pages/home' => 'high_voltage/pages#show', id: 'home'
+
+  # root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
