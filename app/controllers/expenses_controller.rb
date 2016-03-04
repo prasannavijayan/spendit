153134e48c6e940666961923d079eb8f2750a7d3
Prasanna Vijayan <prasanna.vijayan@icicletech.com>
2016-03-04 18:26:16 +0530

    Removed repatition of code

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -77,23 +77,7 @@ class ExpensesController < ApplicationController
 
   private
 
-    # expense view
-    def expense_details
-      @user_budget = UserBudget.new
-      @months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-      next_month_budget
-      this_month_budget
-      @total_expenses = 0
-      @expenses = Expense.all.where(user_id: current_user.id).group_by { |m| m.created_at.month }
-      @month = Time.now.month
-      @date = Time.now
-      @current_user = current_user
-      unless @expenses[@month].nil?
-        @expenses[@month].each do |expense|
-          @total_expenses = @total_expenses + expense.amount
-        end
-      end
-    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_expense
       @expense = Expense.find(params[:id])
@@ -102,52 +86,6 @@ class ExpensesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def expense_params
       params.require(:expense).permit(:title, :amount)
-    end
-
-    # Budget
-    def next_month_budget
-      nextmonth = Time.now.strftime("%m").to_i + 1
-      budgetyear = Time.now.year
-      budgetdate = nextmonth.to_s + budgetyear.to_s
-      @budget_record = UserBudget.where(user_id: current_user.id, budget_date: budgetdate)
-      if @budget_record.blank?
-         if get_last_week_of_this_month.include? Time.now.strftime("%d %b %Y")
-           @next_month_budget = true
-         else
-           @next_month_budget = false
-         end
-      else
-        @next_month_budget = false
-        @budget = @budget_record.first.budget
-      end
-    end
-
-    def this_month_budget
-      budgetdate = Time.now.strftime("%m%Y").to_i
-      @budget_record = UserBudget.where(user_id: current_user.id, budget_date: budgetdate)
-      if @budget_record.count == 0
-        @this_month_budget = true
-      else
-        @this_month_budget = false
-        @budget = @budget_record.first.budget
-      end
-    end
-
-    # Not the best practise need to check and change the code.
-    def get_last_week_of_this_month
-      weeklist = []
-      for i in 0..5 do
-        weeklist << (Date.today.end_of_month - i).strftime("%d %b %Y")
-      end
-      return weeklist
-    end
-
-    def first_two_days_of_this_month
-      first_week = []
-      for i in 0..2 do
-        first_week << (Date.today.end_of_month + i).strftime("%d %b %Y")
-      end
-      return first_week
     end
 
 end
